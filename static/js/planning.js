f396961b107239630f63b6bdca3f399d0c4f7a27
Guillaume Berdeaux <guillaume.berdeaux@epitech.eu>
2018-03-30 16:50:21 +0200

    Changement de l'auto refresh des data, requette toutes les 60sec

--- static/js/planning.js
+++ static/js/planning.js
@@ -15,6 +15,19 @@ var createCard = function(data, room) {
   percent = (!rooms[room]) ? 100 : rooms[room]['percent']
   let intl = new Intl.NumberFormat("arab", {minimumIntegerDigits: 2});
   time = (!rooms[room]) ? "Toute la journée" : rooms[room]["time"].split('.')[0]
+  if (time != "Toute la journée") {
+    let dt = new Date()
+    let time_s = time.split(':')
+    dt.setHours(time_s[0])
+    dt.setMinutes(time_s[1])
+    dt.setSeconds(time_s[2])
+    inc = 1
+    if (taken >= 0.5)
+      inc = -1
+    time = `${intl.format(dt.getHours())}:${intl.format(dt.getMinutes())}:${intl.format(dt.getSeconds() + 1)}`
+    time = "<div inc='" + inc + "' class='time text-center'>" + time + "</div>"
+  } else
+    time = "<div class='text-center'>Toute la journée</div>"
   card = $("<div class='col-12 card-box'>\
               <div class='card text-white bg-" + state + "'>\
                 <div class='card-header'><b style='display: inline-block; width: 80%;white-space: nowrap; overflow: hidden; text-overflow: ellipsis;'>" + room.toTitleCase() + "</b></div>\
@@ -22,8 +35,7 @@ var createCard = function(data, room) {
                   <b style='text-align: center; display: inline-block; width: 100%;white-space: nowrap; overflow: hidden; text-overflow: ellipsis;'>" + acti.toTitleCase() + "</b>\
                   <div class='progress' style='margin-bottom: 5px; background: none; border: 1px solid #fff; height: 10px'>\
                     <div class='progress-bar' style='width: " + percent + "%; background: #fff'></div>\
-                  </div>\
-                  <div class='text-center'>" + time + "</div>\
+                  </div>" + time + "\
                 </div>\
               </div>\
             </div>")
@@ -38,6 +50,17 @@ var setRoom = function(data, room, taken) {
   fill_color = ["#27ae60", "#ffb300", "#e74c3c"]
   createCard(data, room)
   $('#svg svg #' + room).css('fill', fill_color[taken * 2])
+}
+
+var refresh_time = function(elm, inc) {
+  let intl = new Intl.NumberFormat("arab", {minimumIntegerDigits: 2});
+  let time = new Date()
+  let time_s = elm.html().split(':')
+  time.setHours(time_s[0])
+  time.setMinutes(time_s[1])
+  time.setSeconds(time_s[2])
+  time.setSeconds(time.getSeconds() + parseInt(elm.attr('inc')))
+  elm.html(`${intl.format(time.getHours())}:${intl.format(time.getMinutes())}:${intl.format(time.getSeconds())}`)
 }
 
 var api_request = function() {
@@ -60,5 +83,13 @@ var api_request = function() {
   })
 }
 
-window.setInterval(api_request, 1000)
+window.setInterval(api_request, 60000)
+window.setTimeout(function() {
+  window.setInterval(function() {
+    refresh_time($("#hour"), 1)
+    $(".card-box .card-body .time").each(function(id) {
+      refresh_time($(".card-box .card-body .time").eq(id), -1)
+    })
+  }, 1000)
+}, 1000)
 api_request()
